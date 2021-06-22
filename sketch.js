d2ea970f95d680b7c5a135c568f72fd3f46ccd62
rithvikgandesri <86050245+rithvikgandesri@users.noreply.github.com>
2021-06-22 14:35:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var r=0
+var g=0
+var b=0
+function setup(){   
+createCanvas(400,400);
+btn_red=createButton("red");
+btn_red.position(100,50);
+btn_red.mousePressed(red_bg);
+
+btn_green=createButton("green")
+btn_green.position(250,50);
+btn_green.mousePressed(green_bg);
+}
+function red_bg()
+{
+  r=225;
+  g=0;
+  b=0;
+}
+
+function green_bg()
+{
+  r=0;
+  g=225;
+  b=0;
+}
+
+
+function draw(){
+background(r,g,b)}
+
+
+
+
+
+
+
+
